e3bc01b751aa45def4a16b79ab1acf1d79acf44a
Marko Tuononen <marko.tuononen@gmail.com>
2021-04-15 20:04:52 +0300

    Task 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 
-const StatisticLine = ({text, value}) => (<p>{text} {value}</p>)
+const StatisticLine = ({text, value}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
 
 const Statistics = ({good, neutral, bad}) => {
   const all = good+neutral+bad
@@ -9,14 +14,16 @@ const Statistics = ({good, neutral, bad}) => {
 
   if (all) {
     return (
-      <>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive} />
-      </>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
+        </tbody>
+      </table>
     )
   } else {
     return (<p>No feedback given</p>)
