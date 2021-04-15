e17fa5436d035e23cb43c015172d648f68ae1909
Marko Tuononen <marko.tuononen@gmail.com>
2021-04-15 19:45:12 +0300

    Task 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good+neutral+bad
+  const average = (good-bad)/all
+  const positive = 100*good/all
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -16,6 +20,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average || 0}</p>
+      <p>positive {positive || 0}%</p>
     </div>
   )
 }
