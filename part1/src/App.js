13583e08b0f8783395c4e67b0ac1e1cef7f58da6
Marko Tuononen <marko.tuononen@gmail.com>
2021-04-15 18:24:56 +0300

    Task 1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -17,7 +17,7 @@ const Part = (props) => {
 
 const Content = (props) => {
   const content = props.parts.map(part => {
-    return (<Part key={part.name} name={part.name} count={part.count} />)
+    return (<Part key={part.name} name={part.name} count={part.exercises} />)
   })
   return (<div>{content}</div>)
 }
@@ -30,24 +30,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content 
-        parts={[
-          {'name': part1, 'count': exercises1},
-          {'name': part2, 'count': exercises2},
-          {'name': part3, 'count': exercises3},
-        ]}
+        parts={[part1, part2, part3]}
       />
-      <Total count={exercises1 + exercises2 + exercises3} />
+      <Total count={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
