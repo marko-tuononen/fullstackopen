f691c862d5c39e65f76be0490e312b548185e80d
Marko Tuononen <marko.tuononen@gmail.com>
2021-04-18 19:18:45 +0300

    Task 2.6

---
+++ part2/puhelinluettelo/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = event => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+  
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = event => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.id}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
