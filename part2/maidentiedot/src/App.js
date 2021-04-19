478b502b73ae36e64d65c09108a59a3684912261
Marko Tuononen <marko.tuononen@gmail.com>
2021-04-19 14:56:48 +0300

    Task 2.14

--- part2/maidentiedot/src/App.js
+++ part2/maidentiedot/src/App.js
@@ -1,56 +1,65 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Filter = ({filter, onChange}) => {
+const Filter = ({ filter, onChange }) => {
   return (
-    <div>filter shown with <input value={filter} onChange={onChange}/></div>
+    <div>filter shown with <input value={filter} onChange={onChange} /></div>
   )
 }
 
-const ItemRow = ({item, setFilter}) => {
+const CountryRow = ({ country, onShow }) => {
   return (
     <div>
-      {item.name}
-      <button onClick={() => setFilter(item.name)}>show</button>
+      {country.name}
+      <button onClick={() => onShow(country)}>show</button>
     </div>
   )
 }
 
-const ItemDetails = ({item}) => {
+const CountryInfo = ({ basic, weather }) => {
   return (
-    <div>
-      <h1>{item.name}</h1>
-      <p>capital {item.capital}</p>
-      <p>population {item.population}</p>
-      <h2>languages</h2>
-      <ul>
-        {item.languages.map(language => <li>{language.name}</li>)}
-      </ul>
-      <img src={item.flag} width="500"/>
-    </div>
+    <>
+      {basic && (
+        <div>
+          <h1>{basic.name}</h1>
+          <p>capital {basic.capital}</p>
+          <p>population {basic.population}</p>
+          <h2>languages</h2>
+          <ul>
+            {basic.languages.map(language => <li>{language.name}</li>)}
+          </ul>
+          <img src={basic.flag} width="500" />
+          <h2>weather in {basic.capital}</h2>
+          <p>temperature {weather && weather.current.temperature} celcius</p>
+          <p>wind {weather && weather.current.wind_speed} mph direction {weather && weather.current.wind_dir}</p>
+        </div>
+      )}
+    </>
   )
 }
 
-const FilteredList = ({items, filter, setFilter}) => {
-  const matchingItems = items.filter(filter)
-  if (matchingItems.length > 10) {
+const FilteredList = ({ items, onShow }) => {
+  if (items.length > 10) {
     return (<div>too many matches, please specify another filter</div>)
-  } else if (matchingItems.length > 1) {
+  } else if (items.length > 1) {
     return (
       <div>
-        {matchingItems.map(item => <ItemRow key={item.alpha2Code} item={item} setFilter={setFilter} />)}
+        {items.map(item => <CountryRow key={item.alpha2Code} country={item} onShow={onShow} />)}
       </div>
     )
-  } else if (matchingItems.length === 1) {
-    return (<ItemDetails item={matchingItems[0]} />)
+  } else if (items.length === 1) {
+    return (<div />)
   } else {
     return (<div>no matches found, please specify another filter</div>)
   }
 }
 
 const App = () => {
-  const [ countries, setCountries ] = useState([])
-  const [ filter, setFilter ] = useState('')
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+  const [weather, setWeather] = useState(undefined)
+  const [basic, setBasic] = useState(undefined)
+  const [matchingCountries, setMatchingCountries] = useState([])
 
   useEffect(() => {
     axios
@@ -58,16 +67,29 @@ const App = () => {
       .then(response => setCountries(response.data))
   }, [])
 
+  useEffect(() => {
+    const matching = countries.filter(country => country.name.toUpperCase().indexOf(filter.toUpperCase()) !== -1)
+    setMatchingCountries(matching)
+    setBasic(matching.length === 1 ? matching[0] : undefined)
+  }, [filter, countries])
+
+  useEffect(() => {
+    axios
+      .get('/?access_key=' + process.env.REACT_APP_API_KEY + '&query=' + (basic && basic.capital))
+      .then(response => setWeather(response.data))
+  }, [basic])
+
   const handleFilterChange = event => setFilter(event.target.value)
+  const handleShowCountry = country => setBasic(country)
 
   return (
     <div>
       <Filter filter={filter} onChange={handleFilterChange} />
-      <FilteredList 
-        items={countries} 
-        filter={country => country.name.toUpperCase().indexOf(filter.toUpperCase()) !== -1} 
-        setFilter={setFilter}
+      <FilteredList
+        items={matchingCountries}
+        onShow={handleShowCountry}
       />
+      <CountryInfo basic={basic} weather={weather} />
     </div>
   )
 }
